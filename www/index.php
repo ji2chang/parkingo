000a81029516ad9892a94fabeb11fa5a42028a2a
ji2chang <wrz20070109@outlook.com>
2026-02-14 10:40:56 +0100

    redifined routes

--- www/index.php
+++ www/index.php
@@ -7,33 +7,37 @@ require './vendor/autoload.php';
 
 $app = AppFactory::create();
 
-
+$config = require './config/config.php';
 
 // Rotta GET per ottenere una prenotazion
-$app->get('/OttieniPrenotazione', function (Request $request,
+$app->get('/prenotazioni/{codice}', function (Request $request,
                          Response $response,
                          array $args): Response {
-    $response->getBody()->write("ciao");
+    $sql = "SELECT p.nome, p.cognome, p.targa, p.email, p.telefono, p.data_inizio, p.data_fine, p.importo_totale, p.note, p.created_at, p.updated_at, pa.nome ".
+        "FROM prenotazioni p ".
+        "WHERE codice_prenotazione=:codice_prenotazione ".
+        "JOIN parcheggi pa ".
+        "ON p.parcheggio_id=pa.id;";
     return $response;
 });
 
 // Rotta DELETE per rimuovere una prenotazione
-$app->delete('/RimuoviPrenotazione', function (Request $request,
+$app->delete('/prenotazioni/{codice}', function (Request $request,
                          Response $response,
                          array $args): Response {
-    $response->getBody()->write("RimuoviPrenotazione");
+    
     return $response;
 });
 
 // Rotta POST per inserire una nuova prenotazione
-$app->post('/InserisciPrenotazione', function (Request $request,
+$app->post('/prenotazioni', function (Request $request,
                          Response $response,
                          array $args): Response {
     return $response;
 });
 
 // Rotta PUT per aggiornare una prenotazione esistente
-$app->put('/AggiornaPrenotazione', function (Request $request,
+$app->put('/prenotazioni/{codice}', function (Request $request,
                          Response $response,
                          array $args): Response {
     return $response;
