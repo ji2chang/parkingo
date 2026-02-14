595bc4f6798650cae87cbbc76d51d17f6c732504
ji2chang <wrz20070109@outlook.com>
2026-02-14 10:41:06 +0100

    updated config

--- www/config/config.php
+++ www/config/config.php
@@ -7,3 +7,10 @@ const DB_NAME = 'parking_db';
 const DB_USER = 'parking_user';
 
 const DB_PASS = 'parkingpassword';
+
+return [
+    'DB_HOST' => DB_HOST,
+    'DB_NAME' => DB_NAME,
+    'DB_USER' => DB_USER,
+    'DB_PASS' => DB_PASS
+];
