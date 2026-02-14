c34efcecc6b143e12610ea9bc59e18be30567b61
ji2chang <wrz20070109@outlook.com>
2026-02-14 10:29:30 +0100

    added db configuration file

---
+++ www/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+const DB_HOST = 'DATABASE'; //può essere un nome DNS o un indirizzo IP
+
+const DB_NAME = 'parking_db';
+
+const DB_USER = 'parking_user';
+
+const DB_PASS = 'parkingpassword';
